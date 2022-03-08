c0b538fb332d9b97065083404e605af05466247b
Tove <95482627+Vildsvinet@users.noreply.github.com>
2022-03-08 15:35:27 +0100

    Update brot.ex

--- lib/brot.ex
+++ lib/brot.ex
@@ -1,4 +1,20 @@
 defmodule Brot do
   @moduledoc false
 
+  def mandelbrot(c, maxit) do
+    z0 = Cmplx.new(0, 0)
+    it = 0
+    test(it, z0, c, maxit)
+  end
+
+  def test(it, z0, c, maxit) do
+    {ac, bc} = c
+    {az, bz} = Cmplx.square(z0)
+    cond do
+      it == maxit -> 0
+      Cmplx.abs(z0) > 2 -> it
+      true -> test(it+1, {ac+az, bc+bz}, c, maxit)
+    end
+  end
+
 end
